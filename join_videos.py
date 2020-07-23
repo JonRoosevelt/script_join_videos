7419d520871bd38d23c76efd6ea69d52aa7e85e0
JonRoosevelt <jonathanroosevelt@gmail.com>
2020-07-23 15:08:01 -0300

    Adds Exception handling for get_videos method

--- join_videos.py
+++ join_videos.py
@@ -5,9 +5,9 @@ from exceptions import *
 
 
 class JoinVideos:
-    def __init__(self, path, extension="webm", output_name=f"{uuid.uuid4()}.webm"):
+    def __init__(self, path, extension='webm', output_name=f'{uuid.uuid4()}.webm'):
         self.path = path
-        self.extension = f".{extension}"
+        self.extension = f'.{extension}'
         self.output_name = output_name
 
     def get_videos(self):
@@ -20,15 +20,14 @@ class JoinVideos:
                         videos.append(os.path.join(self.path, video))
             return videos[::-1]
         except OSError as err:
+            if error.errno == errno.ENOENT:  # File not found
+                raise OSFileNotFoundException
             raise err
 
     def join_videos(self, videos):
-        command_prefix = f"mkvmerge -o {self.path}/{self.output_name} -w "
-        concat_video_string = " + ".join(videos)
-        return f"{command_prefix}{concat_video_string}"
-
-    def write_videofile(self, joined_videos):
-        joined_videos.write_videofile("")
+        command_prefix = f'mkvmerge -o {self.path}/{self.output_name} -w '
+        concat_video_string = ' + '.join(videos)
+        return f'{command_prefix}{concat_video_string}'
 
     def run(self):
         videos = self.get_videos()
@@ -36,6 +35,5 @@ class JoinVideos:
         print(joined_videos)
         return subprocess.run(joined_videos, shell=True, check=True)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     join_videos = JoinVideos(path=sys.argv[1]).run()
